14b3c3544cff3b5aaf5914ab58d2a652dfeb5b8a
Pau <pauiglesias@gmail.com>
2018-05-12 11:12:16 +0200

    A simple shortcode handler.

--- posts/shortcode.php
+++ posts/shortcode.php
@@ -10,4 +10,33 @@ namespace LittleBizzy\EnableSubtitles\Posts;
  * @subpackage Posts
  */
 class Shortcode {
+
+
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		add_shortcode('the_subtitle', [&$this, 'handler']);
+	}
+
+
+
+	/**
+	 * Handle the shortcode output
+	 */
+	public function handler($atts) {
+
+		// Prepare output
+		$escapeHTML = apply_filters('post_subtitle_esc_html', true);
+
+		// Check post by attributes and retrieve data
+		$subtitle = get_the_subtitle(isset($atts['post'])? $atts['post'] : 0);
+
+		// Done
+		echo $escapeHTML? esc_html($subtitle) : $subtitle;
+	}
+
+
+
 }
